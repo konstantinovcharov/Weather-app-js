ccb5e5d4c5374c111a45efe4a08b57782baaf29e
Konstantin Ovcharov <konstantinovcharov@abv.bg>
2023-03-21 17:40:30 +0200

    get weather function

--- dist/main.main.js
+++ dist/main.main.js
@@ -1,1 +1,1 @@
-console.log("running");
+(()=>{console.log("running");!async function(){const a=await fetch("http://api.openweathermap.org/data/2.5/weather?q=Sofia,BG&appid=214d6c547f73c737ca58bf19a465d50f",{mode:"cors"}),o=await a.json();console.log(o)}()})();
--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 console.log("running")
+
+const Api_Key = "214d6c547f73c737ca58bf19a465d50f";
+
+async function getWeatherData() {
+    const response = await fetch(
+        `http://api.openweathermap.org/data/2.5/weather?q=Sofia,BG&appid=${Api_Key}`, {mode: 'cors'}
+    );
+    const weatherData = await response.json();
+    console.log(weatherData);
+}
+
+getWeatherData();
