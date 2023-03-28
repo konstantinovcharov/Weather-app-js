d4680008e7ec5ea2e6b548881c612e9821841860
Konstantin Ovcharov <konstantinovcharov@abv.bg>
2023-03-28 22:21:38 +0300

    compress images

--- src/index.js
+++ src/index.js
@@ -134,37 +134,37 @@ function switchIcon(rangeId) {
     switch (true) {
         case rangeId >= 200 && rangeId <= 232:
             weatherIconDiv.setAttribute("class", "wi wi-thunderstorm")
-            changeBackgroundImage("center", "cover", "../Images/Thunderstorm.jpg")
+            changeBackgroundImage("center", "cover", "../Images/Thunderstorm-min.jpg")
             break;
 
         case rangeId >= 300 && rangeId <= 321:
             weatherIconDiv.setAttribute("class", "wi wi-sprinkle")
-            changeBackgroundImage("center", "cover", "../Images/Drizzle.jpg")
+            changeBackgroundImage("center", "cover", "../Images/Drizzle-min.jpg")
             break;
 
         case rangeId >= 500 && rangeId <= 531:
             weatherIconDiv.setAttribute("class", "wi wi-rain")
-            changeBackgroundImage("center", "cover", "../Images/Rain.jpg")
+            changeBackgroundImage("center", "cover", "../Images/Rain-min.jpg")
             break;
 
         case rangeId >= 600 && rangeId <= 622:
             weatherIconDiv.setAttribute("class", "wi wi-snow")
-            changeBackgroundImage("center", "cover", "../Images/blizzards.jpg")
+            changeBackgroundImage("center", "cover", "../Images/blizzards-min.jpg")
             break;
 
         case rangeId >= 701 && rangeId <= 781:
             weatherIconDiv.setAttribute("class", "wi wi-fog")
-            changeBackgroundImage("center", "cover", "../Images/foggy.jpg")
+            changeBackgroundImage("center", "cover", "../Images/foggy-min.jpg")
             break;
 
         case rangeId === 800:
             weatherIconDiv.setAttribute("class", "wi wi-day-sunny")
-            changeBackgroundImage("center", "cover", "../Images/clearsky.jpg")
+            changeBackgroundImage("center", "cover", "../Images/clearsky-min.jpg")
             break;
 
         case rangeId >= 801 && rangeId <= 804:
             weatherIconDiv.setAttribute("class", "wi wi-cloudy")
-            changeBackgroundImage("center", "100vw auto", "../Images/cloudy.jpg")
+            changeBackgroundImage("center", "100vw auto", "../Images/cloudy-min.jpg")
             break;
 
     }
